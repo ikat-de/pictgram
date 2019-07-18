8d72afd6b83a74b18d14013258f5c801a31d2c38
”20_Ikeda_Atsushi” <a.ikd05@hotmail.co.jp>
2019-07-18 12:24:17 +0000

    implement strong parameter

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(session_params)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:email)
+  end
+  
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+end
