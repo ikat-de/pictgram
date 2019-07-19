3f61e418ab27d8a22d004ac5f55a042856d81f98
”20_Ikeda_Atsushi” <a.ikd05@hotmail.co.jp>
2019-07-19 07:36:16 +0000

    add limitation of image type(jpg, jpeg, png) and size(under 10 MB)

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,16 @@
+class ImageUploader < CarrierWave::Uploader::Base
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg png)
+  end
+  
+  def size_range
+    1..10.megabytes
+  end
+end
