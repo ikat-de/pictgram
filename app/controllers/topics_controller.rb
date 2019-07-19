04518fef7ac076bd76609eb51bb3730fa55b2477
”20_Ikeda_Atsushi” <a.ikd05@hotmail.co.jp>
2019-07-19 07:39:07 +0000

    do not display post button when logged out

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,31 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+  
+  def new
+    if logged_in? == false
+     redirect_to root_path, danger: '投稿するにはログインしてください'
+    end
+    @topic = Topic.new
+  end
+  
+  def create
+    @topic = current_user.topics.new(topic_params)
+
+    if @topic.save
+      redirect_to topics_path, success: '投稿に成功しました'
+    else
+      flash.now[:danger] = "投稿に失敗しました"
+      render :new
+    end
+  end
+
+  private
+  
+  def topic_params
+      params.require(:topic).permit(:image, :description)
+  end
+end
+
+
